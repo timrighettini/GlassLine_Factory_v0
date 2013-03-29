87645085c1e9a02d92e2e2cc7c88107b695deb7c
timrighettini <tim.righettini@gmail.com>
2013-03-28 20:14:51 -0700

    Added the testcase template

---
+++ src/factory_v0_Tim/test/factoryTests_v0.java
@@ -0,0 +1,19 @@
+package factory_v0_Tim.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class factoryTests_v0 {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void test2() {
+		fail("Not yet implemented");
+	}
+
+}
